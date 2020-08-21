23ac259aabd8763dcce131e49ad528e7add551df
kcprater <kristopherprater@gmail.com>
2020-08-21 10:53:39 -0400

    Add project files.

---
+++ HelpingHand.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HelpingHand.WebMVC.Startup))]
+namespace HelpingHand.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
